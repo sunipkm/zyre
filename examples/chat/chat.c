893ebe0e3bd8aba652249d00ba5b74e79176a570
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2022-09-17 09:45:41 -0600

    Added encryption to chat example?

--- examples/chat/chat.c
+++ examples/chat/chat.c
@@ -22,22 +22,36 @@
 //  DEALINGS IN THE SOFTWARE.
 //  --------------------------------------------------------------------------
 
-
 #include "zyre.h"
 
 //  This actor will listen and publish anything received
 //  on the CHAT group
 
-static void 
-chat_actor (zsock_t *pipe, void *args)
+struct chat_actor_arg_t
 {
-    zyre_t *node = zyre_new ((char *) args);
+    char *name;
+    bool encryption;
+};
+
+static void chat_actor (zsock_t *pipe, void *_args)
+{
+    struct chat_actor_arg_t *args = (struct chat_actor_arg_t *)_args;
+    zyre_t *node = zyre_new (args->name);
     if (!node)
-        return;                 //  Could not create new node
+        return; //  Could not create new node
+
+    zcert_t *cert = NULL;
+    if (args->encryption) {
+        cert = zcert_new ();
+        assert (cert);
+        zyre_set_zcert (node, cert);
+        zcert_destroy(&cert);
+    }
 
     zyre_start (node);
     zyre_join (node, "CHAT");
-    zsock_signal (pipe, 0);     //  Signal "ready" to caller
+    zsock_signal (pipe, 0); //  Signal "ready" to caller
+    zyre_print(node);
 
     bool terminated = false;
     zpoller_t *poller = zpoller_new (pipe, zyre_socket (node), NULL);
@@ -46,25 +60,21 @@ chat_actor (zsock_t *pipe, void *args)
         if (which == pipe) {
             zmsg_t *msg = zmsg_recv (which);
             if (!msg)
-                break;              //  Interrupted
+                break; //  Interrupted
 
             char *command = zmsg_popstr (msg);
             if (streq (command, "$TERM"))
                 terminated = true;
-            else
-            if (streq (command, "SHOUT")) { 
+            else if (streq (command, "SHOUT")) {
                 char *string = zmsg_popstr (msg);
                 zyre_shouts (node, "CHAT", "%s", string);
-            }
-            else {
+            } else {
                 puts ("E: invalid message to actor");
                 assert (false);
             }
             free (command);
             zmsg_destroy (&msg);
-        }
-        else
-        if (which == zyre_socket (node)) {
+        } else if (which == zyre_socket (node)) {
             zmsg_t *msg = zmsg_recv (which);
             char *event = zmsg_popstr (msg);
             char *peer = zmsg_popstr (msg);
@@ -74,17 +84,13 @@ chat_actor (zsock_t *pipe, void *args)
 
             if (streq (event, "ENTER"))
                 printf ("%s has joined the chat\n", name);
-            else
-            if (streq (event, "EXIT"))
+            else if (streq (event, "EXIT"))
                 printf ("%s has left the chat\n", name);
-            else
-            if (streq (event, "SHOUT"))
+            else if (streq (event, "SHOUT"))
                 printf ("%s: %s\n", name, message);
-            else
-            if (streq (event, "EVASIVE"))
+            else if (streq (event, "EVASIVE"))
                 printf ("%s is being evasive\n", name);
-            else
-            if (streq (event, "SILENT"))
+            else if (streq (event, "SILENT"))
                 printf ("%s is being silent\n", name);
 
             free (event);
@@ -99,23 +105,28 @@ chat_actor (zsock_t *pipe, void *args)
     zyre_stop (node);
     zclock_sleep (100);
     zyre_destroy (&node);
+    zclock_sleep (100);
 }
 
-int
-main (int argc, char *argv [])
+int main (int argc, char *argv[])
 {
     if (argc < 2) {
-        puts ("syntax: ./chat myname");
+        puts ("syntax: ./chat myname [encryption]");
         exit (0);
     }
-    zactor_t *actor = zactor_new (chat_actor, argv [1]);
+    struct chat_actor_arg_t arglist = {.name = argv[1], .encryption = false};
+    if (argc == 3)
+    {
+        arglist.encryption = true;
+    }
+    zactor_t *actor = zactor_new (chat_actor, &arglist);
     assert (actor);
-    
+
     while (!zsys_interrupted) {
-        char message [1024];
+        char message[1024];
         if (!fgets (message, 1024, stdin))
             break;
-        message [strlen (message) - 1] = 0;     // Drop the trailing linefeed
+        message[strlen (message) - 1] = 0; // Drop the trailing linefeed
         zstr_sendx (actor, "SHOUT", message, NULL);
     }
     zactor_destroy (&actor);
